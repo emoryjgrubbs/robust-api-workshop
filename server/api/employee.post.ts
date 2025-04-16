ce36a1890bc0086740063ef08355df002332c881
Emory Grubbs <emoryjgrubbs@gmail.com>
2025-04-16 17:35:34 -0500

    added prisma errors

--- server/api/employee.post.ts
+++ server/api/employee.post.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Prisma } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -29,21 +29,61 @@ export default defineEventHandler(async (event) => {
 
     const { fName, lName, phoneNum, dNum } = validatedBody.data;
 
-    const user = await prisma.user.create({
-        data: {
-            fName: fName,
-            lName: lName,
-            phoneNum: phoneNum,
-        }
-    });
+    try {
+        const user = await prisma.user.create({
+            data: {
+                fName: fName,
+                lName: lName,
+                phoneNum: phoneNum,
+            }
+        });
 
-    const employee = await prisma.employee.create({
-        data: {
-            id: user.id,
-            dNum: dNum
-        }
-    });
+        const employee = await prisma.employee.create({
+            data: {
+                id: user.id,
+                dNum: dNum
+            }
+        });
 
-    return {user, employee};
+        return {user, employee};
+    }
+    catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+			switch (String(e.code)) {
+				case "P2002":
+					console.log("uniqueness error");
+					throw createError({
+						statusCode: 400,
+						statusMessage: "Schema Uniqueness Constraint Violated",
+						data: e.meta,
+					});
+				case "P2005":
+				case "P2006":
+					console.log("bad input");
+					throw createError({
+						statusCode: 400,
+						statusMessage: "Wrong input type",
+						data: e.meta,
+					});
+				case "P2011":
+				case "P2012":
+				case "P2013":
+					console.log("missing required fields");
+					throw createError({
+						statusCode: 400,
+						statusMessage: "Missing required fields",
+						data: e.meta,
+					});
+				default:
+					throw createError({
+						statusCode: 400,
+						statusMessage: "Unexpected Prisma error",
+						message: e.message,
+						data: e.meta,
+					});
+			}
+		}
+    }
+
 });
 
--- server/api/order.post.ts
+++ server/api/order.post.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Prisma } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -29,14 +29,53 @@ export default defineEventHandler(async (event) => {
 
     const { sId, pId, time, quantity } = validatedBody.data;
 
-    const order = await prisma.order.create({
-        data: {
-            sId: sId,
-            pId: pId,
-            time: time,
-            quantity: quantity
-        }
-    });
+    try {
+        const order = await prisma.order.create({
+            data: {
+                sId: sId,
+                pId: pId,
+                time: time,
+                quantity: quantity
+            }
+        });
 
-    return order;
+        return order;
+    }
+    catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+			switch (String(e.code)) {
+				case "P2002":
+					console.log("uniqueness error");
+					throw createError({
+						statusCode: 400,
+						statusMessage: "Schema Uniqueness Constraint Violated",
+						data: e.meta,
+					});
+				case "P2005":
+				case "P2006":
+					console.log("bad input");
+					throw createError({
+						statusCode: 400,
+						statusMessage: "Wrong input type",
+						data: e.meta,
+					});
+				case "P2011":
+				case "P2012":
+				case "P2013":
+					console.log("missing required fields");
+					throw createError({
+						statusCode: 400,
+						statusMessage: "Missing required fields",
+						data: e.meta,
+					});
+				default:
+					throw createError({
+						statusCode: 400,
+						statusMessage: "Unexpected Prisma error",
+						message: e.message,
+						data: e.meta,
+					});
+			}
+		}
+    }
 });
